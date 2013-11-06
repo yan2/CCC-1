f0be5d7b89e825a06f713cba658100481e46b370
yan2 <niou@TTS-120271ML.local>
2013-11-06 12:57:25 -0500

    added go home button to game over

--- CCC_physics/Sprites/GameOverLayer.mm
+++ CCC_physics/Sprites/GameOverLayer.mm
@@ -11,9 +11,9 @@
 @implementation GameOverLayer
 @synthesize iPad;
 
-//- (void)onHome: (id) sender {
-//    [SceneManager goGameScene];
-//}
+- (void)onHome: (id) sender {
+    [SceneManager goMainMenu];
+}
 //
 //
 //- (void)addBackButton {
@@ -48,29 +48,34 @@
         CGSize screenSize = [CCDirector sharedDirector].winSize;
         
         // Calculate Large Font Size
-        int largeFont = screenSize.height / 9;
+        int largeFont = screenSize.height / 12;
         
         // Create a label
         CCLabelTTF *label = [CCLabelTTF labelWithString:@"Game Over!"
                                                fontName:@"Marker Felt"
                                                fontSize:largeFont];
 		// Center label
-		label.position = ccp( screenSize.width/2, screenSize.height/2);
+		label.position = ccp( screenSize.width/2, screenSize.height/3*2);
         
         // Add label to this scene
 		[self addChild:label z:0];
         
         // Set font settings
         [CCMenuItemFont setFontName:@"Marker Felt"];
-        [CCMenuItemFont setFontSize:16];
+        [CCMenuItemFont setFontSize:30];
         
         // Create font based items ready for CCMenu
         CCMenuItemFont *item1 = [CCMenuItemFont itemFromString:@"Replay" target:self selector:@selector(onReplay:)];
-                CCMenu *menu = [CCMenu menuWithItems:item1, nil];
+        CCMenuItemImage *goHome = [CCMenuItemImage itemFromNormalImage:@"Home icon.png"
+                            selectedImage:@"Home icon.png"
+                           target:self
+                           selector:@selector(onHome:)];
+        goHome.scale = .5;
+                CCMenu *menu = [CCMenu menuWithItems:item1,goHome, nil];
         
         // Align the menu
         [menu alignItemsVertically];
-        menu.position = ccp(screenSize.width/2,screenSize.height/3);
+        menu.position = ccp(screenSize.width/2,screenSize.height/2-20);
         // Add the menu to the scene
         [self addChild:menu];
         //  Put a 'back' button in the scene
