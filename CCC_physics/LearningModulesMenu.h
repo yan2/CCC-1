567ee582338f474019f55932b4f6c706a1a77b5a
yan2 <niou@wl-10-190-235-91.wireless.duke.edu>
2013-11-06 15:43:13 -0500

    create learning modules page

---
+++ CCC_physics/LearningModulesMenu.h
@@ -0,0 +1,16 @@
+//
+//  LearningModulesMenu.h
+//  CCC_physics
+//
+//  Created by Ann Niou on 11/6/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface LearningModulesMenu : CCLayer {
+    
+}
+
+@end
