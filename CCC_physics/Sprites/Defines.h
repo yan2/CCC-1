e6f3dde46d6dc4668ee61446e0aeb97fb3f757ac
xurui203 <xurui203@gmail.com>
2013-11-06 14:15:17 -0500

    Fixed mis sync in Defines.h

--- CCC_physics/Sprites/Defines.h
+++ CCC_physics/Sprites/Defines.h
@@ -19,12 +19,23 @@
 #define frandom (float)arc4random()/UINT64_C(0x100000000)
 #define frandom_range(low,high) ((high-low)*frandom)+low
 
+
+// font sizes
+
+#define kFontScaleHuge 6;
+#define kFontScaleLarge 9;
+#define kFontScaleMedium 10;
+#define kFontScaleSmall 12;
+#define kFontScaleTiny 14;
+
+
 // 3 - enumerations
 typedef enum _ActionState {
     kActionStateNone = 0,
     kActionStateIdle,
     kActionStateSuperPower,
     kActionStateWalk,
+    kActionStateCrawl,
     kActionStateFail
 } ActionState;
 
@@ -34,5 +45,7 @@ typedef struct _BoundingBox {
     CGRect original;
     
 } BoundingBox;
+
+
 
 #endif
